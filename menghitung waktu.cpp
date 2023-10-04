9009ac831b2d08ae4ae847a9a29edf594df75cff
pradawira2786 <141219035+pradawira2786@users.noreply.github.com>
2023-10-04 07:39:43 +0700

    Add files via upload

---
+++ menghitung waktu.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    int s;
+    int v;
+    double sinA;
+    double waktu;
+
+	cout << "Masukkan sudut peluncuran (0-90 derajat): ";
+    cin >> s;
+    cout << "Masukkan kecepatan awal: ";
+    cin >> v;
+    sinA = sin((double) (s * 22) / 7 / 180);
+    waktu = 2 * (v * sinA) / 10;
+    cout << "Waktu yang ditempuh Boro untuk mencapai jarak terjauh adalah" << waktu << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
